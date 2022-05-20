c28b28f92d163a633cec4f921d6fff263aa3cdcb
kennethliem <kennethliem991@gmail.com>
2022-05-20 12:11:59 +0700

    Camerapage & Previewpage - Layout

---
+++ app/src/main/java/com/capstone/guideme/ui/preview/PreviewActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.guideme.ui.preview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.guideme.R
+
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
